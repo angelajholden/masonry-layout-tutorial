e0c49cfd140155ea94d9dc5e5cfe83479a86822e
Angela J Holden <angelajholden@gmail.com>
2025-07-15 15:00:51 -0500

    lazy loading and js filter

---
+++ script.js
@@ -0,0 +1,28 @@
+function articleFilter() {
+	const buttons = document.querySelectorAll(".button");
+	const articles = document.querySelectorAll(".article-item");
+
+	buttons.forEach((button) => {
+		button.addEventListener("click", () => {
+			const category = button.getAttribute("data-category");
+
+			// Update active button
+			buttons.forEach((btn) => btn.classList.remove("active"));
+			button.classList.add("active");
+
+			// Filter Articles
+			articles.forEach((article) => {
+				const articleCategory = article.getAttribute("data-category");
+
+				if (category === "all" || category === articleCategory) {
+					article.style.display = "";
+				} else {
+					article.style.display = "none";
+				}
+			});
+		});
+	});
+}
+document.addEventListener("DOMContentLoaded", () => {
+	articleFilter();
+});
