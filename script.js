9a5f9f70023f43e5abccbdcbc753f3a397da621b
Angela J Holden <angelajholden@gmail.com>
2025-07-15 20:09:35 -0500

    adding aria-pressed for a11y

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function articleFilter() {
 			const category = button.getAttribute("data-category");
 
 			// Update active button
-			buttons.forEach((btn) => btn.classList.remove("active"));
+			buttons.forEach((btn) => {
+				btn.classList.remove("active");
+				btn.setAttribute("aria-pressed", "false");
+			});
 			button.classList.add("active");
+			button.setAttribute("aria-pressed", "true");
 
 			// Filter Articles
 			articles.forEach((article) => {
